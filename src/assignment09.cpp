cf19c201411bda333d24b7b8640c34ff3bdf086f
t o <finalfantasytimothy@gmail.com>
2019-03-08 21:39:29 -0700

    Fixes to Test4 and uncomment Test3

--- src/assignment09.cpp
+++ src/assignment09.cpp
@@ -31,7 +31,7 @@ void testBalance();
 #define TEST1   // for testSimple()
 #define TEST2   // for testAdd()
 #define TEST3   // for testIterate()
-//#define TEST4   // for testDelete()
+#define TEST4   // for testDelete()
 //#define TESTA   // for testBalance()
 
 /**********************************************************************
